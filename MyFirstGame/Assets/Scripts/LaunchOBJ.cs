04587607499df42c24d9a266160a6d65395d2dc0
Firo <onca5001@gmail.com>
2020-02-18 12:11:34 +0000

    Beta_01

---
+++ MyFirstGame/Assets/Scripts/LaunchOBJ.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchOBJ : MonoBehaviour
+{
+
+    public GameObject PetrolpiecePreFAB;
+    public Transform EngineBlockSPWN;
+    public Transform[] TransformTargetList;
+    private Transform Target;
+     private int NT;
+
+    public float Speed;
+    public float spawnspeed;
+    private float ogspawnspeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindTargets();
+        ogspawnspeed = spawnspeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnspeed -= Time.deltaTime;
+        if (spawnspeed <= 0)
+        {
+            GameObject NewP;
+            NewP = Instantiate(PetrolpiecePreFAB, EngineBlockSPWN.position, EngineBlockSPWN.rotation);
+            NewP.AddComponent<Rigidbody>().AddForce(Target.position -transform.position * Speed, ForceMode.Impulse);
+            spawnspeed = ogspawnspeed;
+            Nexttarget();
+            
+        }
+    }
+
+    void Nexttarget()
+    {
+        NT = Random.Range(0, TransformTargetList.Length);
+        Debug.Log(NT);
+        Target = TransformTargetList[NT];
+    }
+
+    void FindTargets()
+    {
+        GameObject[] FoundTargets;
+          FoundTargets = GameObject.FindGameObjectsWithTag("PetrolTarget");
+        foreach (GameObject i in FoundTargets)
+        {
+            Vector3 positions;
+            positions = i.transform.position;
+
+
+        }
+        
+        
+
+    }
+
+}
